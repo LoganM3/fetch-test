75cf4309e173c545a008645e03a2333666d7af0b
Logan McCalley <mccalleylogan@gmail.com>
2022-12-20 10:32:34 -0500

    form filled out, post not working

--- src/App.js
+++ src/App.js
@@ -1,47 +1,56 @@
 import Form from "react-bootstrap/Form";
 import { useState, useEffect } from "react";
 import './App.css';
+import { Button } from "react-bootstrap";
 
 function App() {
 
   const [form, setForm] = useState({});
-  const [info, setInfo] = useState()
+   const [info, setInfo] = useState()
 
   useEffect (() => {
     fetch('https://frontend-take-home.fetchrewards.com/form')
     .then(res => res.json())
     .then(data => setInfo(data))
     .catch(err => console.log(err))
-  }, [setInfo])
+  }, [])
 
   //console.log(info)
 
-  // const occupations = info?.occupations.map ((occupation, index) =>{
-  //   return(
-  //   <div>key={index} value={occupation.id}</div>,
-  //   {occupation}
-  //   )});
+   const handleSubmit = (e) => {
+     e.preventDefault();
+     fetch('https://frontend-take-home.fetchrewards.com/form'
+    , {
+      method: "POST",
+      mode: "cors",
+      headers: { 
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    })
+     .catch(err => alert(err.message))
+  }
 
-
-  // console.log(occupations)
 
   const updateForm = (e) => {
     console.log({ [e.target.name]: e.target.value });
     setForm({ ...form, [e.target.name]: e.target.value });
   };
+  
+  console.log(form)
 
   return (
     <div className="App">
-  <Form>
+  <Form action="submit">
   <Form.Group className="Name" controlId="">
               <Form.Label>Full Name</Form.Label>
               <Form.Control
                 type="text"
                 name="name"
                 placeholder="add name"
-                required={true}
-                // value={form.name}
                 onChange={updateForm}
+                 value={form.name}
+                required="true"
               />
             </Form.Group>
          
@@ -52,8 +61,9 @@ function App() {
                 type="email"                         
                 name="email"
                 placeholder="example@test.com"
-                required={true}
+                 required="true"
                 onChange={updateForm}
+                value={form.email}
               />
             </Form.Group> 
 
@@ -61,47 +71,49 @@ function App() {
               <Form.Label>Password</Form.Label>
               <Form.Control 
               type="password" 
-              name="inputPassword5"
-              aria-describedby="passwordHelpBlock"
+              name="Password"
               placeholder="password" 
-              required={true}
+               required="true"
               onChange={updateForm}
+              value={form.password}
               />
               
             </Form.Group>
 
             <Form.Group className="dropDown" controlId="">
-           
-            
-              <Form.Label>ocupation</Form.Label>
-             <Form.Select>
-               <option>Open this select menu</option>
-                
-                 {info?.occupations.map((job, index) =>(
-                   <option key={index} value={job.occupation} >
-                     {job.occupation}
+              <Form.Label>occupation</Form.Label>
+             <Form.Select 
+              required="true"  
+             name="occupation"
+            onChange={updateForm}
+             value={form.occupation}
+             >
+               <option >-select job-</option>
+                 {info?.occupations.map((occupation, index) =>(
+                   <option key={index} value={occupation} >
+                     {occupation}
                    </option>
                      ))}  
-             {/* <option value="1">One</option>
-             <option value="2">Two</option>  */}
-              onChange={updateForm}
              </Form.Select>
             </Form.Group>
            
             <Form.Group className="dropDown" controlId="">
               <Form.Label>State</Form.Label>
-             <Form.Select>
-             <option>Select your state</option>
-
+             <Form.Select 
+            required="true"  
+             name="state"
+            onChange={updateForm}
+             value={form.state}
+             >
+             <option>-Select your state-</option>
              {info?.states.map((state, index) =>(
                    <option key={index} value={state.name} >
                      {state.name}
                    </option>
                      ))}  
              </Form.Select>
-            </Form.Group>
-  
-
+             </Form.Group>
+              <Button type="submit" onClick={handleSubmit}> Submit </Button>
           </Form>
     </div>
   );
